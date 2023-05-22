98357aae6ad31df1e99f021dd38528602b4a1f4d
Saveli Shulzhenka <Saveli.shulz@gmail.com>
2023-05-22 23:25:30 +0300

    2

---
+++ HomeWork/Hw01/Task02/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int MaxNumber(int num1, int num2, int num3)
+{
+    int maxNum = Math.Max(num1, Math.Max(num2, num3));
+    Console.WriteLine($"Максимальное число это {maxNum}");
+
+    return maxNum;
+}
+
+MaxNumber(Convert.ToInt32(Console.ReadLine()),
+          Convert.ToInt32(Console.ReadLine()),
+          Convert.ToInt32(Console.ReadLine()));
---
+++ HomeWork/Hw01/Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+int EvenNumbers(int num1)
+{
+    if(num1 % 2 == 0) Console.WriteLine($"Число {num1}, является четным");
+    else Console.WriteLine($"Число {num1}, не является четным");
+
+    return 0;
+}
+
+EvenNumbers(Convert.ToInt32(Console.ReadLine()));
